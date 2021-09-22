c4f74561926f889a796bc4ac48d6b579037adcab
Putra Pratama Nasution <putrapratamanst@gmail.com>
2021-09-22 11:42:40 +0700

    prod

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1:3307;dbname=sidik',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_fce95287d37007b',
+    'username' => 'b0cb04836be83c',
+    'password' => '6945a88f',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
