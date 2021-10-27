80542f360cebf6a8eb8693eb6921f8f720b3db39
Putra Pratama Nasution <putrapratamanst@gmail.com>
2021-10-28 01:40:19 +0700

    fix view data

--- views/peserta/list.php
+++ views/peserta/list.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\helpers\Url;
+
 // use kartik\grid\GridView;
 
 
@@ -180,10 +182,16 @@ if (!Yii::$app->user->isGuest) {
                     [
                         'class' => 'yii\grid\ActionColumn',
                         'template' => '{view}',
+                        'buttons' => [
+                            'view' => function ($url, $model) {
+                                $url = Url::to(['peserta/view', 'id' => $model->id]);
+                               return Html::a('<svg aria-hidden="true" style="display:inline-block;font-size:inherit;height:1em;overflow:visible;vertical-align:-.125em;width:1.125em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M573 241C518 136 411 64 288 64S58 136 3 241a32 32 0 000 30c55 105 162 177 285 177s230-72 285-177a32 32 0 000-30zM288 400a144 144 0 11144-144 144 144 0 01-144 144zm0-240a95 95 0 00-25 4 48 48 0 01-67 67 96 96 0 1092-71z"></path></svg>', $url, ['title' => 'view']);
+                            },
+                         ]
                     ],
                 ],
             ]);
         }
     }
     ?>
-</div>
+</div>  
